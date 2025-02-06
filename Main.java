970cf868c26a7a69e0a6db8133b716497617ef7a
cina3 <cinar.genc@ug.bilkent.edu.tr>
2025-02-06 09:43:54 +0300

    Added methods

--- Main.java
+++ Main.java
@@ -1,4 +1,36 @@
 public class Main {
+    public static void printMenu() {
+
+    }
+
+    public static int[] createRandomArray(int arraySize) {
+       
+    }
+
+    public static int findMax(int[] arr) {
+        
+    }
+
+    public static int findMin(int[] arr) {
+       
+    }
+
+    public static double calculateAverage(int[] arr) {
+        
+    }
+
+    public static double[] calculateDeviations(int[] arr) {
+        
+    }
+
+    public static int calculateSumOfEvenIndexes(int[] arr) {
+      
+    }
+
+    public static int calculateSumOfOddIndexes(int[] arr) {
+       
+    }
+
     public static void main(String[] args) {
         
     }
